7a01b9b495024565a2c4a9b28f8b4ad07017ca85
cn14 <chethan.171co113@nitk.edu.in>
2020-07-30 09:28:30 +0530

    Updates

---
+++ precache-manifest.cf82955addd451f2d5da7837c4809c01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c96da4c363f4c587b303e58800f45b6",
+    "url": "/COVID-19-tracker/index.html"
+  },
+  {
+    "revision": "4a8f13b70c5d3cc86971",
+    "url": "/COVID-19-tracker/static/css/main.a046b43e.chunk.css"
+  },
+  {
+    "revision": "942d4c7578e3bad4995f",
+    "url": "/COVID-19-tracker/static/js/2.7b7cea3c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-tracker/static/js/2.7b7cea3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8f13b70c5d3cc86971",
+    "url": "/COVID-19-tracker/static/js/main.baa0870e.chunk.js"
+  },
+  {
+    "revision": "1993e5d0498649478c36",
+    "url": "/COVID-19-tracker/static/js/runtime-main.5a6ef8d4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-tracker/static/media/image.d7265326.png"
+  }
+]);
